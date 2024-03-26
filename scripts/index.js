77c9f3929563ec7a044c63d8f35c0a8af9a67f3c
Grigori Turovchik <Jeronimoq49@yandex.ru>
2024-03-26 18:17:21 +0300

    1.1

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,25 @@ const cardContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 function createCard(cardContent,deleteHandler) {
   const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
-  const cardButton = cardItem.querySelector('.card__delete-button');
+  const cardButtonDelete = cardItem.querySelector('.card__delete-button');
   cardItem.querySelector('.card__title').textContent = cardContent.name;
   cardItem.querySelector('.card__image').src = cardContent.link;
   cardItem.querySelector('.card__image').alt = cardContent.name;
-  cardButton.addEventListener("click", function(element){
+  cardButtonDelete.addEventListener("click", function(element){
     deleteHandler(element)
   });
   return cardItem;
 }
 
 // @todo: Функция удаления карточки
- function cardDelete(item) {
+ function deleteCard(item) {
   item.target.closest('.places__item').remove();
 }
 
 // @todo: Вывести карточки на страницу
 function createCards() {
   initialCards.forEach(function(cardContent){
-    cardContainer.append(createCard(cardContent,cardDelete))
+    cardContainer.append(createCard(cardContent,deleteCard))
   })
 }
 
